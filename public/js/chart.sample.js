2ec790797537e1e2cd4125aa7c378bfe0bc9521d
Matthew E. Blancada <meblancada@panasiaticsolutions.net>
2022-04-23 22:01:55 +0800

    forth commit

--- public/js/chart.sample.js
+++ public/js/chart.sample.js
@@ -1,73 +1,69 @@
-const randomChartData = n => {
-  let data = []
+"use strict";
 
-  for (let i = 0; i < n; i++) {
-    data.push(Math.round(Math.random() * 200))
+var randomChartData = function randomChartData(n) {
+  var data = [];
+
+  for (var i = 0; i < n; i++) {
+    data.push(Math.round(Math.random() * 200));
   }
 
-  return data
-}
+  return data;
+};
 
-const chartColors = {
-  default: {
+var chartColors = {
+  "default": {
     primary: '#00D1B2',
     info: '#209CEE',
     danger: '#FF3860'
   }
-}
-
-const ctx = document.getElementById('big-line-chart').getContext('2d');
-
+};
+var ctx = document.getElementById('big-line-chart').getContext('2d');
 new Chart(ctx, {
   type: 'line',
   data: {
-    datasets: [
-      {
-        fill: false,
-        borderColor: chartColors.default.primary,
-        borderWidth: 2,
-        borderDash: [],
-        borderDashOffset: 0.0,
-        pointBackgroundColor: chartColors.default.primary,
-        pointBorderColor: 'rgba(255,255,255,0)',
-        pointHoverBackgroundColor: chartColors.default.primary,
-        pointBorderWidth: 20,
-        pointHoverRadius: 4,
-        pointHoverBorderWidth: 15,
-        pointRadius: 4,
-        data: randomChartData(9)
-      },
-      {
-        fill: false,
-        borderColor: chartColors.default.info,
-        borderWidth: 2,
-        borderDash: [],
-        borderDashOffset: 0.0,
-        pointBackgroundColor: chartColors.default.info,
-        pointBorderColor: 'rgba(255,255,255,0)',
-        pointHoverBackgroundColor: chartColors.default.info,
-        pointBorderWidth: 20,
-        pointHoverRadius: 4,
-        pointHoverBorderWidth: 15,
-        pointRadius: 4,
-        data: randomChartData(9)
-      },
-      {
-        fill: false,
-        borderColor: chartColors.default.danger,
-        borderWidth: 2,
-        borderDash: [],
-        borderDashOffset: 0.0,
-        pointBackgroundColor: chartColors.default.danger,
-        pointBorderColor: 'rgba(255,255,255,0)',
-        pointHoverBackgroundColor: chartColors.default.danger,
-        pointBorderWidth: 20,
-        pointHoverRadius: 4,
-        pointHoverBorderWidth: 15,
-        pointRadius: 4,
-        data: randomChartData(9)
-      }
-    ],
+    datasets: [{
+      fill: false,
+      borderColor: chartColors["default"].primary,
+      borderWidth: 2,
+      borderDash: [],
+      borderDashOffset: 0.0,
+      pointBackgroundColor: chartColors["default"].primary,
+      pointBorderColor: 'rgba(255,255,255,0)',
+      pointHoverBackgroundColor: chartColors["default"].primary,
+      pointBorderWidth: 20,
+      pointHoverRadius: 4,
+      pointHoverBorderWidth: 15,
+      pointRadius: 4,
+      data: randomChartData(9)
+    }, {
+      fill: false,
+      borderColor: chartColors["default"].info,
+      borderWidth: 2,
+      borderDash: [],
+      borderDashOffset: 0.0,
+      pointBackgroundColor: chartColors["default"].info,
+      pointBorderColor: 'rgba(255,255,255,0)',
+      pointHoverBackgroundColor: chartColors["default"].info,
+      pointBorderWidth: 20,
+      pointHoverRadius: 4,
+      pointHoverBorderWidth: 15,
+      pointRadius: 4,
+      data: randomChartData(9)
+    }, {
+      fill: false,
+      borderColor: chartColors["default"].danger,
+      borderWidth: 2,
+      borderDash: [],
+      borderDashOffset: 0.0,
+      pointBackgroundColor: chartColors["default"].danger,
+      pointBorderColor: 'rgba(255,255,255,0)',
+      pointHoverBackgroundColor: chartColors["default"].danger,
+      pointBorderWidth: 20,
+      pointHoverRadius: 4,
+      pointHoverBorderWidth: 15,
+      pointRadius: 4,
+      data: randomChartData(9)
+    }],
     labels: ['01', '02', '03', '04', '05', '06', '07', '08', '09']
   },
   options: {
@@ -99,7 +95,6 @@ new Chart(ctx, {
           fontColor: '#9a9a9a'
         }
       }],
-
       xAxes: [{
         barPercentage: 1.6,
         gridLines: {
@@ -114,6 +109,4 @@ new Chart(ctx, {
       }]
     }
   }
-})
-
-
+});
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,159 +7,183 @@
         <title>Laravel</title>
 
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
+        <link rel="stylesheet" type="text/css" href="{{ asset('css/main.min.css') }}">
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
         <link rel="stylesheet" href="https://unpkg.com/bulma@0.9.0/css/bulma.min.css" />
         <!-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@5.15.4/css/fontawesome.min.css" /> -->
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
+     
     </head>
     <body>
-        <nav class="navbar is-dark">
-        <div class="container">
-            <div class="navbar-brand">
-                <a class="navbar-item brand-text" href="/mainpage">
-            
-        </a>
-                <div class="navbar-burger burger" data-target="navMenu">
-                    <span></span>
-                    <span></span>
-                    <span></span>
+        <div id="app">
+                        <nav id="navbar-main" class="navbar is-fixed-top">
+                <div class="navbar-brand">
+                  <a class="navbar-item is-hidden-desktop jb-aside-mobile-toggle">
+                    <span class="icon"><i class="mdi mdi-forwardburger mdi-24px"></i></span>
+                  </a>
+                  <div class="navbar-item has-control">
+                    <div class="control"><input placeholder="Search everywhere..." class="input"></div>
+                  </div>
                 </div>
-            </div>
-            <div id="navMenu" class="navbar-menu">
-                <div class="navbar-start">
-                    <a class="navbar-item" href="/mainpage">
-           <i class="fa fa-home"></i> <h3 style="margin-left: 5px;">Home</h3>
-          </a>
-                    <a class="navbar-item" href="/product">
-            <i class="fa fa-product-hunt"></i> <h3 style="margin-left: 5px;">Product</h3>
-          </a>
-                <!--     <a class="navbar-item" href="/orders">
-            Orders
-          </a> -->
-                   <!--  <a class="navbar-item" href="/payments">
-            Payments
-          </a>
-                                 <a class="navbar-item" href="/reports">
-            Reports
-          </a> -->
+                <div class="navbar-brand is-right">
+                  <a class="navbar-item is-hidden-desktop jb-navbar-menu-toggle" data-target="navbar-menu">
+                    <span class="icon"><i class="mdi mdi-dots-vertical"></i></span>
+                  </a>
                 </div>
-
-            </div>
-
-      <div class="navbar-item has-dropdown is-hoverable">
-        <a class="navbar-link" style="color: white;">
-          {{ session('username') }}
-        </a>
-
-        <div class="navbar-dropdown">
-          <a href="/logout" class="navbar-item">
-            Logout
-          </a>
-         <!--  <a class="navbar-item">
-            Jobs
-          </a>
-          <a class="navbar-item">
-            Contact
-          </a>
-          <hr class="navbar-divider">
-          <a class="navbar-item">
-            Report an issue
-          </a> -->
-        </div>
+                <div class="navbar-menu fadeIn animated faster" id="navbar-menu">
+                  <div class="navbar-end">
+                    <div class="navbar-item has-dropdown has-dropdown-with-icons has-divider is-hoverable">
+                      <a class="navbar-link is-arrowless">
+                        <span class="icon"><i class="mdi mdi-menu"></i></span>
+                        <span>Sample Menu</span>
+                        <span class="icon">
+                        <i class="mdi mdi-chevron-down"></i>
+                      </span>
+                      </a>
+                      <div class="navbar-dropdown">
+                        <a href="profile.html" class="navbar-item">
+                          <span class="icon"><i class="mdi mdi-account"></i></span>
+                          <span>My Profile</span>
+                        </a>
+                        <a class="navbar-item">
+                          <span class="icon"><i class="mdi mdi-settings"></i></span>
+                          <span>Settings</span>
+                        </a>
+                        <a class="navbar-item">
+                          <span class="icon"><i class="mdi mdi-email"></i></span>
+                          <span>Messages</span>
+                        </a>
+                        <hr class="navbar-divider">
+                        <a class="navbar-item">
+                          <span class="icon"><i class="mdi mdi-logout"></i></span>
+                          <span>Log Out</span>
+                        </a>
+                      </div>
+                    </div>
+                    <div class="navbar-item has-dropdown has-dropdown-with-icons has-divider has-user-avatar is-hoverable">
+                      <a class="navbar-link is-arrowless">
+                        <div class="is-user-avatar">
+                          <img src="https://avatars.dicebear.com/v2/initials/john-doe.svg" alt="John Doe">
+                        </div>
+                        <div class="is-user-name"><span>John Doe</span></div>
+                        <span class="icon"><i class="mdi mdi-chevron-down"></i></span>
+                      </a>
+                      <div class="navbar-dropdown">
+                        <a href="profile.html" class="navbar-item">
+                          <span class="icon"><i class="mdi mdi-account"></i></span>
+                          <span>My Profile</span>
+                        </a>
+                        <a class="navbar-item">
+                          <span class="icon"><i class="mdi mdi-settings"></i></span>
+                          <span>Settings</span>
+                        </a>
+                        <a class="navbar-item">
+                          <span class="icon"><i class="mdi mdi-email"></i></span>
+                          <span>Messages</span>
+                        </a>
+                        <hr class="navbar-divider">
+                        <a class="navbar-item">
+                          <span class="icon"><i class="mdi mdi-logout"></i></span>
+                          <span>Log Out</span>
+                        </a>
+                      </div>
+                    </div>
+                    <a href="https://justboil.me/bulma-admin-template/one-html" title="About" class="navbar-item has-divider is-desktop-icon-only">
+                      <span class="icon"><i class="mdi mdi-help-circle-outline"></i></span>
+                      <span>About</span>
+                    </a>
+                    <a title="Log out" class="navbar-item is-desktop-icon-only">
+                      <span class="icon"><i class="mdi mdi-logout"></i></span>
+                      <span>Log out</span>
+                    </a>
+                  </div>
+                </div>
+              </nav>
+                  <aside class="aside is-placed-left is-expanded">
+    <div class="aside-tools">
+      <div class="aside-tools-label">
+        <span><b>Admin</b> One HTML</span>
       </div>
     </div>
-        </div>
-    </nav>
-  
-    <!-- <div class="container"> -->
-        @yield('content')
-    <!-- </div> -->
-       
-        <!-- <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
+    <div class="menu is-menu-main">
+      <p class="menu-label">General</p>
+      <ul class="menu-list">
+        <li>
+          <a href="index.html" class="is-active router-link-active has-icon">
+            <span class="icon"><i class="mdi mdi-desktop-mac"></i></span>
+            <span class="menu-item-label">Dashboard</span>
+          </a>
+        </li>
+      </ul>
+      <p class="menu-label">Examples</p>
+      <ul class="menu-list">
+        <li>
+          <a href="tables.html" class="has-icon">
+            <span class="icon has-update-mark"><i class="mdi mdi-table"></i></span>
+            <span class="menu-item-label">Tables</span>
+          </a>
+        </li>
+        <li>
+          <a href="forms.html" class="has-icon">
+            <span class="icon"><i class="mdi mdi-square-edit-outline"></i></span>
+            <span class="menu-item-label">Forms</span>
+          </a>
+        </li>
+        <li>
+          <a href="profile.html" class="has-icon">
+            <span class="icon"><i class="mdi mdi-account-circle"></i></span>
+            <span class="menu-item-label">Profile</span>
+          </a>
+        </li>
+        <li>
+          <a class="has-icon has-dropdown-icon">
+            <span class="icon"><i class="mdi mdi-view-list"></i></span>
+            <span class="menu-item-label">Submenus</span>
+            <div class="dropdown-icon">
+              <span class="icon"><i class="mdi mdi-plus"></i></span>
             </div>
-        </div> -->
+          </a>
+          <ul>
+            <li>
+              <a href="#void">
+                <span>Sub-item One</span>
+              </a>
+            </li>
+            <li>
+              <a href="#void">
+                <span>Sub-item Two</span>
+              </a>
+            </li>
+          </ul>
+        </li>
+      </ul>
+      <p class="menu-label">About</p>
+      <ul class="menu-list">
+        <li>
+          <a href="https://admin-one-html.justboil.me/" target="_blank" class="has-icon">
+            <span class="icon"><i class="mdi mdi-credit-card-outline"></i></span>
+            <span class="menu-item-label">Premium Demo</span>
+          </a>
+        </li>
+        <li>
+          <a href="https://justboil.me/bulma-admin-template/one-html" class="has-icon">
+            <span class="icon"><i class="mdi mdi-help-circle"></i></span>
+            <span class="menu-item-label">About</span>
+          </a>
+        </li>
+      </ul>
+    </div>
+  </aside>
+                   @yield('content')
+        </div>
+   
+        <script type="text/javascript" src="{{ asset('js/main.min.js') }}"></script>
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.min.js"></script>
+<script type="text/javascript" src="{{ asset('js/chart.sample.min.js') }}"></script>
+
+<!-- Icons below are for demo only. Feel free to use any icon pack. Docs: https://bulma.io/documentation/elements/icon/ -->
+<link rel="stylesheet" href="https://cdn.materialdesignicons.com/4.9.95/css/materialdesignicons.min.css">
     </body>
 </html>
